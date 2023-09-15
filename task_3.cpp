a22b44553945af242a7533c4d38c01dcfa458686
Sergey Lyulchenko <solo300m@yandex.ru>
2023-09-15 09:35:33 +0400

    task-3

--- task_3.cpp
+++ task_3.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 using namespace std;
+int evendingits(long long number, int &ans)
+{
+    if (number / 10 == 0)
+    {
+        if (number % 2 == 0)
+        {
+            ans++;
+        }
+    }
+    else
+    {
+        int tmp = number % 10;
+        if (tmp % 2 == 0)
+            ans++;
+        number = number / 10;
+        evendingits(number, ans);
+    }
+    return 1;
+}
 int main()
 {
+    long long a = 1373955553557;
+    int ans = 0;
+    evendingits(a, ans);
+    cout << "Количество четных цифр: " << ans << endl;
 }
